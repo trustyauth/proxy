22779d1a8bc15fd0115e39f718e4fb5211e21a90
Tyler McGinnis <tylermcginnis@gmail.com>
2024-07-27 22:13:44 -0400

    Add support for yaml config

--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
 
 func main() {
-	fmt.Println("picket")
+	if err := run(os.Args[1:]); err != nil {
+		slog.Error("something went wrong", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
+	fs := flag.NewFlagSet("picket", flag.ContinueOnError)
+	configFlag := fs.String("config", "", "config path")
+	err := fs.Parse(args)
+	if err != nil {
+		return err
+	}
+
+	_, err = ReadConfig(*configFlag)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Config represents a picket configuration file
+type Config struct {
+	// Address to listen for requests
+	Addr string `yaml:"addr"`
+
+	// List of origin servers to forward requests to
+	Origins []*OriginConfig `yaml:"origins"`
+}
+
+// OriginConfig represents a configuration that maps a relative path to an origin server
+type OriginConfig struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
+// ReadConfig unmarshals the config from a file
+func ReadConfig(filename string) (c Config, err error) {
+	buf, err := os.ReadFile(filename)
+	if os.IsNotExist(err) {
+		err = fmt.Errorf("missing config file: %s", filename)
+		return
+	} else if err != nil {
+		return
+	}
+
+	err = yaml.Unmarshal(buf, &c)
+	if err != nil {
+		return
+	}
+
+	return
 }
---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestReadConfig(t *testing.T) {
+	t.Run("ReadConfig", func(t *testing.T) {
+		configFile := filepath.Join(t.TempDir(), "picket.yml")
+		if err := os.WriteFile(configFile, []byte(`
+addr: localhost:80
+origins:
+  - path: /
+    url: http://localhost:3000
+  - path: /api
+    url: http://localhost:8080
+`[1:]), fs.ModePerm); err != nil {
+			t.Fatal(err)
+		}
+
+		c, err := ReadConfig(configFile)
+		if err != nil {
+			t.Fatal(err)
+		} else if got, want := c.Addr, "localhost:80"; got != want {
+			t.Fatalf("Addr=%v, want %v", got, want)
+		} else if got, want := c.Origins[0].Path, "/"; got != want {
+			t.Fatalf("Origin.Path=%v, want %v", got, want)
+		} else if got, want := c.Origins[0].URL, "http://localhost:3000"; got != want {
+			t.Fatalf("Origin.URL=%v, want %v", got, want)
+		}
+	})
+}
